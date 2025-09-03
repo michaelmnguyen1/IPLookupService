c8d298cb2c5a93967e5a00b99c91d34870399bca
michaelmnguyen1 <nguyen.m.michael@gmail.com>
2025-09-03 13:46:26 -0700

    Delete CapCountHandlers directory

--- CapCountHandlers/src/main/java/com/getcake/capcount/model/DBTransactionInfo.java
+++
@@ -1,8 +0,0 @@
-package com.getcake.capcount.model;
-
-import com.datastax.driver.core.BatchStatement;
-
-public class DBTransactionInfo {
-    public BatchStatement batchUpsertIncCountStmt, batchUpdateIncDateStmt, batchInsertIncReqStmt, batchDeleteDateStmt, batchDeleteCountStmt;
-
-}
