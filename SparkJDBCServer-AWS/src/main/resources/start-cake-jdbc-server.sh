3ce7b280fc9b1df9581fde786e4b218e4f9e2e4b
michaelmnguyen1 <nguyen.m.michael@gmail.com>
2025-09-03 13:47:18 -0700

    Delete SparkJDBCServer-AWS directory

--- SparkJDBCServer-AWS/src/main/resources/start-cake-jdbc-server.sh
+++
@@ -1,6 +0,0 @@
-clear
-export PATH=/home/ec2-user/jdk/bin:$PATH:$HOME/bin
-export JAVA_HOME=/home/ec2-user/jdk
-
-/cakeapps/spark/bin/spark-submit --conf spark.root.logger=ALL,FILE --jars ./cake-jars/guava-11.0.2.jar,./cake-jars/aws-java-sdk-core-1.10.16.jar,./cake-jars/aws-java-sdk-s3-1.10.16.jar,./cake-jars/hadoop-aws-2.6.0.jar,./cake-jars/jetty-server-9.0.2.v20130417.jar,./cake-jars/jetty-util-9.0.2.v20130417.jar,./cake-jars/jetty-http-9.0.2.v20130417.jar,./cake-jars/jetty-io-9.0.2.v20130417.jar,./cake-jars/spark-csv_2.11-1.2.0.jar,./cake-jars/commons-csv-1.1.jar,./cake-jars/jtds-1.3.1.jar,./cake-jars/joda-time-2.8.1.jar --properties-file cake-spark-jdbc-server.conf.qa.s3.sparkMaster  --class com.getcake.sparkjdbc.SparkJDBCServer /cakeapps/cake-spark-jdbc-server/cake-spark-jdbc-server.jar cake-spark-jdbc-server.conf.qa.s3.sparkMaster > cake-jdbc-server.log &
-
--- SparkJDBCServer-AWS/src/main/scripts/start-cake-jdbc-server.sh
+++
@@ -1,3 +0,0 @@
-/home/ec2-user/spark-1.4.0-bin-hadoop2.6/sbin/start-master.sh &
-/home/ec2-user/spark-1.4.0-bin-hadoop2.6/sbin/start-slave.sh spark://ip-10-128-1-78:7077 &
-/home/ec2-user/spark-1.4.0-bin-hadoop2.6/bin/spark-submit --jars ./cake-jars/guava-11.0.2.jar,./cake-jars/aws-java-sdk-core-1.10.16.jar,./cake-jars/aws-java-sdk-s3-1.10.16.jar,./cake-jars/hadoop-aws-2.6.0.jar --properties-file cake-spark-jdbc-server.conf.qa.localfiles.sparkMaster  --class com.getcake.sparkjdbc.SparkJDBCServer ./cake-spark-jdbc-server-09-09-15.jar cake-spark-jdbc-server.conf.qa.localfiles.sparkMaster &
